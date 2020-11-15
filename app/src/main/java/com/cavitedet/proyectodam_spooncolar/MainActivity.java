b6a6e44c3f4ad20e94ecd5e7c4e819ca7946fa59
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2020-11-15 13:40:15 +0100

    Initial commit

---
+++ app/src/main/java/com/cavitedet/proyectodam_spooncolar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cavitedet.proyectodam_spooncolar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

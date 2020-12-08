55130783f925f2eadabe54cc02165b9267c3132f
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2020-12-08 17:39:17 +0100

    Arreglado fixture para pasar un test

--- app/src/testShared/java/infrastructure/local/fixtures/NutricionFixture.java
+++ app/src/testShared/java/infrastructure/local/fixtures/NutricionFixture.java
@@ -4,11 +4,14 @@ import com.cavitedet.proyectodam_spoonacular.domain.modelos.ingrediente_detallad
 import com.cavitedet.proyectodam_spoonacular.domain.modelos.ingrediente_detallado.Propiedad;
 import com.cavitedet.proyectodam_spoonacular.infrastructure.repositorio.local.room_models.ingredienteDetallado.NutricionRoomDto;
 import com.cavitedet.proyectodam_spoonacular.infrastructure.repositorio.remoto.json_modelos.ingrediente_detallado.NutricionGsonDto;
+import com.cavitedet.proyectodam_spoonacular.infrastructure.repositorio.remoto.json_modelos.ingrediente_detallado.PropiedadGsonDto;
 
 import java.util.Arrays;
 import java.util.Collections;
 
 public class NutricionFixture extends PesoUnidadFixture {
+
+
     protected Nutricion getNutricionDominio() {
         // Para testear el orden
         Propiedad prop2 = getPropiedadDominio();
@@ -33,9 +36,13 @@ public class NutricionFixture extends PesoUnidadFixture {
 
 
     protected NutricionGsonDto getNutricionGsonDto() {
+        PropiedadGsonDto prop2 = getPropiedadGsonDto();
+        prop2.setNombre("a");
+        prop2.setCantidad(0.0);
+        prop2.setUnidad("g");
         NutricionGsonDto dto = new NutricionGsonDto();
         dto.setNutrientes(Collections.singletonList(getNutrienteGsonDto()));
-        dto.setPropiedades(Collections.singletonList(getPropiedadGsonDto()));
+        dto.setPropiedades(Arrays.asList(getPropiedadGsonDto(), prop2));
         dto.setDescomposicionCalorica(getDecomposicionCaloricaGsonDto());
         dto.setPesoPorRacion(getPesoPorUnidadGsonDto());
         return dto;
